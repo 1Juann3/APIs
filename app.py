ecd351e1ee6ad30bb1005260e3af43c5b7287cb5
1Juann3 <1juannx3>
2020-07-18 16:07:04 -0500

    added routes

--- app.py
+++ app.py
@@ -2,8 +2,36 @@ from flask import Flask
 
 app = Flask(__name__) 
 
-@app.route('/')
-def home():
-    return "Hello, world"
+stores = [
+    {
+        'name': 'store1'
+        'items':[
+            {
+                'name': 'item1'
+                'price': 10.99
+            }
+        ]
+    }
+]
+
+@app.route('/store', methods=['POST'])
+def create_store():
+    pass
+
+@app.route('/store/<string:name>')
+def get_store_name(name):
+    pass
+
+@app.route('/store')
+def get_stores():
+    pass
+
+@app.route('/store/<sting:name>/item', method=['POST'])
+def create_item(name):
+    pass
+
+@app.route('/store/<string:name>/item')
+def get_store_items(name):
+    pass
 
 app.run(port = 5000)
